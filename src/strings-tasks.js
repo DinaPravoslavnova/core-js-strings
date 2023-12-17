ccdafbd6c9fabe8135a63653d4e8e8b92e8b9c1d
Dina P <dinchik@bk.ru>
2023-12-18 01:35:25 +0400

    feat: add solution for the 5st task strings-tasks block

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -88,8 +88,9 @@ function getFirstChar(value) {
  *   removeLeadingAndTrailingWhitespaces('cat ') => 'cat'
  *   removeLeadingAndTrailingWhitespaces('\t\t\tHello, World! ') => 'Hello, World!'
  */
-function removeLeadingAndTrailingWhitespaces(/* value */) {
-  throw new Error('Not implemented');
+function removeLeadingAndTrailingWhitespaces(value) {
+  const resultRemoveTrailing = value.trim();
+  return resultRemoveTrailing;
 }
 
 /**
