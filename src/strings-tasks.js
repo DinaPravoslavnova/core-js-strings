b77d738bfc6978e3f4d9cbbf3a5356cfb392f73e
Dina P <dinchik@bk.ru>
2023-12-19 03:27:02 +0400

    feat: add solution for the 14th task strings-tasks block

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -258,8 +258,11 @@ function endsWith(str, substr) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const resultFormatTime = `${String(minutes).padStart(2, '0')}:${String(
+    seconds
+  ).padStart(2, '0')}`;
+  return resultFormatTime;
 }
 
 /**
