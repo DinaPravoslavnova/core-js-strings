12615c5ab8fd981bbc6609fd270d8c8078cbb589
Dina P <dinchik@bk.ru>
2023-12-19 11:05:16 +0400

    feat: add solution for the 26th task strings-tasks block

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -487,8 +487,9 @@ function unbracketTag(str) {
  *   ],
  *   'info@gmail.com' => ['info@gmail.com']
  */
-function extractEmails(/* str */) {
-  throw new Error('Not implemented');
+function extractEmails(str) {
+  const resultArrayEmails = str.split(';');
+  return resultArrayEmails;
 }
 
 /**
