e118db99cb57242c24575c81d30481a1dfe3a068
Dina P <dinchik@bk.ru>
2023-12-19 10:51:15 +0400

    feat: add solution for the 24th task strings-tasks block

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -451,8 +451,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   extractNameFromTemplate('Hello, John Doe!') => 'John Doe'
  *   extractNameFromTemplate('Hello, Chuck Norris!') => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const resultName = value.slice(7, -1);
+  return resultName;
 }
 
 /**
