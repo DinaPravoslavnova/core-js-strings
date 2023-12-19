aac32a14c58b0aacdab84f418a0fe3a4b21f15e2
Dina P <dinchik@bk.ru>
2023-12-19 05:14:37 +0400

    feat: add solution for the 18th task strings-tasks block

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -331,8 +331,13 @@ function containsSubstring(str, substring) {
  *   countVowels('aEiOu') => 5
  *   countVowels('XYZ') => 1
  */
-function countVowels(/* str */) {
-  throw new Error('Not implemented');
+function countVowels(str) {
+  const vowels = 'AEIOUYaeiouy';
+  let resultCountVowels = 0;
+  for (let i = 0; i < str.length; i += 1) {
+    if (vowels.includes(str[i])) resultCountVowels += 1;
+  }
+  return resultCountVowels;
 }
 
 /**
